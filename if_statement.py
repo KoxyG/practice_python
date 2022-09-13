4b3d731187a1a3ccfe497a5a51e1a305b1995e10
unknown <koxygraphics@gmail.com>
2022-09-13 16:54:15 +0100

    done

---
+++ if_statement.py
@@ -0,0 +1,19 @@
+#sam teach yourself python "Hour 3"
+a = 5
+if a < 1:
+    print(a)
+
+
+#printing a name with if statement using if condition
+name = "Doug"
+if name == "Doug":
+    print("Hello, D-man!." + " " + "How are you today?")
+
+
+#adding and else to an if
+a = 5
+if a > 5:
+    print("Greater than 5")
+else:
+    print("less than 5") #you can also use ellipse to represent it returning zero
+
